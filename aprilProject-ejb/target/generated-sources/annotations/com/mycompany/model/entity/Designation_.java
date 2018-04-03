f3f4d03251f146ebe5f3ea1e84edc0bf4d1d2bc3
RamenSayami <ramen.sayami@gmail.com>
2018-04-03 11:28:55 +0545

    added gitignore

--- aprilProject-ejb/target/generated-sources/annotations/com/mycompany/model/entity/Designation_.java
+++ aprilProject-ejb/target/generated-sources/annotations/com/mycompany/model/entity/Designation_.java
@@ -4,7 +4,7 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2018-04-02T16:56:45")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2018-04-03T11:20:57")
 @StaticMetamodel(Designation.class)
 public class Designation_ { 
 
--- aprilProject-ejb/target/generated-sources/annotations/com/mycompany/model/entity/Staff_.java
+++ aprilProject-ejb/target/generated-sources/annotations/com/mycompany/model/entity/Staff_.java
@@ -4,7 +4,7 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2018-04-03T10:04:19")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2018-04-03T11:20:57")
 @StaticMetamodel(Staff.class)
 public class Staff_ { 
 
